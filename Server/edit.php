68a5f1ed6f4537738842248c16474299777e352b
Stephan Ruiz <sdr-1998@hotmail.com>
2018-06-15 17:33:15 +0200

    added upload and edit the edit page a bit more

--- Server/edit.php
+++ Server/edit.php
@@ -14,15 +14,16 @@ if (isset($_POST['submit'])) {
     $name = sanitize(preg_replace("/[^A-Za-z0-9 ]/", '', mb_substr($_POST['name'], 0, 100)));
     $desc = sanitize(mb_substr($_POST['description'], 0, 500));
     $id = $project->id;
-    
-    $target_dir = "uploads/projects/";
+    $token = generate_token($name);
+
+    $target_dir = $_SERVER['DOCUMENT_ROOT'] . "/img/";
     $target_file = $target_dir . basename($_FILES["uploadFile"]["name"]);
     move_uploaded_file($_FILES["uploadFile"]["tmp_name"], $target_file);
-    $picture = "/Server/img/" . basename($_FILES["uploadFile"]["name"]);
-    
+    $picture = "/img/" . basename($_FILES["uploadFile"]["name"]);
+
     $sql = "UPDATE `projects` SET `token`='{$token}', `name`='{$name}', `desc`='{$desc}', `picture`='{$picture}' WHERE `projects`.`id`={$id}";
     $conn->query($sql);
-    
+
 //    header("Location: index.php");
 //    exit;
 }
@@ -50,7 +51,13 @@ if (isset($_POST['submit'])) {
         <div class="container">
             <div class="row">
                 <div class="col s2">
-                    <img src="uploads/<?= sanitize($project->picture) ?>" alt="<?= sanitize($project->name) ?>" class="circle responsive-img" style="width: 100px; height: 100px;">
+                    <?php
+                    echo $project->picture;
+                    if ($project->picture == "projects/default.png") { ?>
+                        <img src="uploads/<?= sanitize($project->picture) ?>" alt="<?= sanitize($project->name) ?>" class="circle responsive-img" style="width: 100px; height: 100px;">
+                    <?php } else { ?>
+                        <img src="<?= sanitize($project->picture) ?>" alt="<?= sanitize($project->name) ?>" class="circle responsive-img" style="width: 100px; height: 100px;">
+                    <?php } ?>
                 </div>
                 <div class="col s10">
                     <h2>Project <span style="color: #4caf50"><?= sanitize($project->name) ?></span></h2>
@@ -59,11 +66,11 @@ if (isset($_POST['submit'])) {
 
                 <h6>"<?= sanitize($project->desc) ?>"</h6>
 
-                <form action="edit.php?id=<?= sanitize($project->id) ?>" method="POST" enctype="multipart/form-data">
+                <form action="edit.php?id=<?= sanitize($project->id) ?>" method="POST" enctype="multipart/form-data" id="<?= $project->id ?>">
                     <input type="file" name="uploadFile">
                     <input type="text" name="name">
-                    <input type="text" name="description">
-                    <input type="submit" name="submit">
+                    <textarea rows="4" cols="50" name="description" placeholder="Right a short description of the project.."  style="overflow:auto;resize:none"></textarea>
+                    <input type="submit" name="submit" value="Submit!">
                 </form>
             </div>
         </div>
